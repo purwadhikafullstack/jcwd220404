08f0bb818e8d98089ae9cb909be5dd6275a9d243
fachrizahidaya <111639685+fachrizahidaya@users.noreply.github.com>
2022-12-16 09:21:30 +0700

    feat: database and models (#3)
    * done merge
    
    * feat: models and database

---
+++ projects/server/models/inventory.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Inventory extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Inventory.belongsTo(models.Admin);
+      Inventory.belongsTo(models.Branch);
+      Inventory.belongsTo(models.Product);
+    }
+  }
+  Inventory.init(
+    {
+      stockQty: DataTypes.INTEGER,
+      entryDate: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Inventory",
+    }
+  );
+  return Inventory;
+};
