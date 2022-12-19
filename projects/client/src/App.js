686fa0f42eb7903203c79e10d4d7076997c5f1db
ErikLewis95 <eriksimaremare@gmail.com>
2022-12-19 15:46:34 +0700

    Register Verification done

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -5,42 +5,10 @@ import { NotificationPage } from "./pages/NotificationPage";
 import { TransactionPage } from "./pages/TransactionPage";
 import { CategoryPage } from "./pages/CategoryPage";
 import { CartPage } from "./pages/CartPage";
-import { LoginPage } from "./pages/LoginPage";
-import { ResetPassPage } from "./pages/ResetPasswordPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { VerificationPage } from "./pages/VerificationPage"
-import { useEffect } from "react";
-import { useDispatch } from "react-redux"
-import Axios from "axios";
-import { login } from "./redux/userSlice"
 
 function App() {
-  const dispatch = useDispatch();
-  const token = localStorage.getItem("token");
-
-  const keepLogin = async () => {
-    try {
-      const res = await Axios.get(`http://localhost:8000/user/keepLogin`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      // console.log(res.data);
-      dispatch(login({
-        id: res.data.id,
-        phoneNumber: res.data.phoneNumber,
-        email: res.data.email,
-        profilePic: res.data.profilePic,
-        isVerified: res.data.isVerified,
-      }));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-useEffect(() => {
-  keepLogin()
-})
 
   return (
     <div className="App">
@@ -51,10 +19,9 @@ useEffect(() => {
         <Route path="/transaction" element={<TransactionPage/>}></Route>
         <Route path="/category" element={<CategoryPage/>}></Route>
         <Route path="/cart" element={<CartPage/>}></Route>
-        <Route path="/login" element={<LoginPage/>}></Route>
         <Route path="/register" element={<RegisterPage/>}></Route>
-        <Route path="/verification/:token" element={<VerificationPage />} />
-        <Route path="/resetpassword/:token" element={<ResetPassPage />} />
+        <Route path="/verification/:token" element={<VerificationPage />}></Route>
+        
       </Routes>
     </div>
   );
