e2fa9e84f43bdc1f76e4a4f002ac794790591549
ErikLewis95 <eriksimaremare@gmail.com>
2022-12-19 16:47:13 +0700

    feat:Register Verification

--- projects/server/models/user.js
+++ projects/server/models/user.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: "username",
+        unique: "name",
     },
     email: {
         type: DataTypes.STRING,
