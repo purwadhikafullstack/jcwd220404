855c350f1c7ebd21e66fd01bce6422bcccd2155e
fachrizahidaya <fachriza.theblues@gmail.com>
2023-02-02 16:07:29 +0700

    updated

--- projects/client/src/redux/nameSlice.js
+++ projects/client/src/redux/nameSlice.js
@@ -1,21 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  name: "admin",
-  value: [],
-};
+// const initialState = {
+//   name: "admin",
+//   value: [],
+// };
 
-export const nameSlice = createSlice({
-  name: "admin",
-  initialState,
-  reducers: {
-    syncData: (state, action) => {
-      state.value = action.payload;
-    },
-  },
-});
+// export const nameSlice = createSlice({
+//   name: "admin",
+//   initialState,
+//   reducers: {
+//     syncData: (state, action) => {
+//       state.value = action.payload;
+//     },
+//   },
+// });
 
-// Action creators are generated for each case reducer function
-export const { syncData } = nameSlice.actions;
-
-export default nameSlice.reducer;
+// // Action creators are generated for each case reducer function
+// export const { syncData } = nameSlice.actions;
+// export default nameSlice.reducer;
