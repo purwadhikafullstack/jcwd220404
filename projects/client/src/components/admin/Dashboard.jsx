1325342d3d63fd51636d705af445be251aa0d053
fachrizahidaya <fachriza.theblues@gmail.com>
2023-02-07 17:49:39 +0700

    updated

--- projects/client/src/components/admin/Dashboard.jsx
+++ projects/client/src/components/admin/Dashboard.jsx
@@ -12,35 +12,135 @@ import { MdOutlinePayments } from "react-icons/md";
 import { GoPackage } from "react-icons/go";
 import { TbTruckDelivery } from "react-icons/tb";
 import { MdDoneOutline } from "react-icons/md";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import { useSelector } from "react-redux";
 
 export const DashboardComp = () => {
+  const [data, setData] = useState();
+  const [data2, setData2] = useState();
+  const [data3, setData3] = useState();
+  const [data4, setData4] = useState();
+  const [data5, setData5] = useState();
+  const [data6, setData6] = useState();
+  const { id } = useSelector((state) => state.adminSlice.value);
+
+  const getData = async () => {
+    try {
+      const result = await Axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/transaction/listWaitingPayment/${id}`
+      );
+      setData(result.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [id]);
+
+  const getData2 = async () => {
+    try {
+      const result = await Axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/transaction/listConfirmPayment/${id}`
+      );
+      setData2(result.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData2();
+  }, [id]);
+
+  const getData3 = async () => {
+    try {
+      const result = await Axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/transaction/listOnProcess/${id}`
+      );
+      setData3(result.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData3();
+  }, [id]);
+
+  const getData4 = async () => {
+    try {
+      const result = await Axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/transaction/listDelivery/${id}`
+      );
+      setData4(result.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData4();
+  }, [id]);
+
+  const getData5 = async () => {
+    try {
+      const result = await Axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/transaction/listDone/${id}`
+      );
+      setData5(result.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData5();
+  }, [id]);
+
+  const getData6 = async () => {
+    try {
+      const result = await Axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/transaction/listCancelled/${id}`
+      );
+      setData6(result.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData6();
+  }, [id]);
   return (
     <div>
       <Box maxW="6xl" pl={20} mx={"auto"} pt={50}>
         <SimpleGrid columns={{ base: 1, md: 5 }} spacing={{ base: 5, lg: 2 }}>
           <StatsCard
             title={"Waiting Payment"}
-            stat={"0"}
+            stat={data?.length}
             icon={<MdOutlinePayment size={"2.5em"} />}
           />
           <StatsCard
             title={"Confirm Payment"}
-            stat={"0"}
+            stat={data2?.length}
             icon={<MdOutlinePayments size={"2.5em"} />}
           />
           <StatsCard
             title={"On Process"}
-            stat={"0"}
+            stat={data3?.length}
             icon={<GoPackage size={"2.5em"} />}
           />
           <StatsCard
             title={"Delivery"}
-            stat={"0"}
+            stat={data4?.length}
             icon={<TbTruckDelivery size={"2.5em"} />}
           />
           <StatsCard
             title={"Done"}
-            stat={"0"}
+            stat={data5?.length}
             icon={<MdDoneOutline size={"2.5em"} />}
           />
         </SimpleGrid>
