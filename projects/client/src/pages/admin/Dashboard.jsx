de8a2be44d750dab6f8fcb0d1cf215ad978b1bcb
fachrizahidaya <fachriza.theblues@gmail.com>
2023-02-09 14:54:58 +0700

    updated

--- projects/client/src/pages/admin/Dashboard.jsx
+++ projects/client/src/pages/admin/Dashboard.jsx
@@ -1,8 +1,73 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Tooltip } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { DashboardComp } from "../../components/admin/Dashboard";
 import { SidebarComp } from "../../components/admin/Sidebar";
+import Axios from "axios";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 export const DashboardPage = () => {
+  const [data7, setData7] = useState();
+  const [data8, setData8] = useState();
+  const [branch, setBranch] = useState();
+  const { id } = useSelector((state) => state.adminSlice.value);
+
+  const getBranch = async () => {
+    try {
+      const res = await Axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/branch/adminByBranch/${id}`
+      );
+      setBranch(res.data);
+      console.log(res.data);
+      setData7(res.data.id);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getBranch();
+  }, [id]);
+
+  const findStock = async () => {
+    try {
+      const stock = await Axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/inventory/find/${data7}`
+      );
+      console.log(stock.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    findStock();
+  }, [data7]);
+
+  const getInv = async () => {
+    try {
+      const res = await Axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/inventory/findAllByBranch/${data7}`
+      );
+      setData8(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getInv();
+  }, [data7]);
+
   return (
     <div>
       <Grid
@@ -22,8 +87,28 @@ export const DashboardPage = () => {
         </GridItem>
         <GridItem>
           <DashboardComp />
+        <BarChart
+          width={50}
+          height={50}
+          data={data8}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          {/* <XAxis dataKey="ProductId" />
+          <YAxis />
+          <Tooltip dataKey={"totalQty"} />
+          <Legend dataKey={"totalQty"} />
+          <Bar dataKey={"totalQty"} fill="#8884d8" /> */}
+        </BarChart>
         </GridItem>
       </Grid>
+      <Box>
+      </Box>
     </div>
   );
 };
--- projects/server/src/controllers/admin/inventoryController.js
+++ projects/server/src/controllers/admin/inventoryController.js
@@ -396,4 +396,32 @@ module.exports = {
       res.status(400).send(err);
     }
   },
+
+  totalInvAll: async (req, res) => {
+    try {
+      const total = await inventory.findAll({
+        attributes: [
+          "BranchId",
+          [Sequelize.fn("count", Sequelize.col("ProductId")), "total_product"],
+        ],
+        group: ["BranchId"],
+        include: [{ model: branch }],
+      });
+      console.log(total);
+      const salesInv = total.map((item) => item.dataValues.total_product);
+      console.log(salesInv)
+
+      // let numberSalesTotal = [];
+      // length = salesTotal.length;
+      // for (let i = 0; i < length; i++)
+      //   numberSalesTotal.push(parseInt(salesTotal[i]));
+      // console.log(numberSalesTotal);
+      res.status(200).send({
+        total,
+        // numberSalesTotal,
+      });
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  },
 };
